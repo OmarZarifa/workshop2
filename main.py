9d27b67f310b49b29023ea66a640a21cd34fa6ee
Omar Zarifa <omarzarifa@gmail.com>
2023-02-06 17:32:20 +0100

    last change

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 def main():
     name = input("Enter your name: ")
     print(f"Hello {name}")
-<<<<<<< HEAD
-=======
-
-def hello():
-    print('hello')
-
-def omar():
-    print("طول بالك")
->>>>>>> db0ea6356286d0c58f00ff72eb1bf6175b1ca36d
-    
 
 if __name__ == '__main__':
     main()
