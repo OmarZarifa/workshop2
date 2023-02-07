71f19111e9cad1e1a94dd9e6f59e2551ac46f686
Omar Zarifa <omarzarifa@gmail.com>
2023-02-07 01:32:17 +0100

    Print some info

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
+def name():
     name = input("Enter your name: ")
-    print(f"Hello {name}")
+    return name
+    
+def age():
+    age = int(input("Enter your age: "))
+    return age
+
+def get_info(name, age):
+    print(f"Your name is: {name} and your age is {age}")
+
+def main():
+    n = name()
+    a = age()
+    get_info(n, a)
+
 
 if __name__ == '__main__':
     main()
